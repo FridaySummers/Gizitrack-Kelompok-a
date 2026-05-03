22112566826d936f7eaf48c26eb8b499e1f37020
Sanjaya Fathur Rahman <sanjayafathurrahman@student.telkomuniversity.ac.id>
2026-05-03 13:10:22 +0700

    feat: realistic database seeder for GiziTrack

---
+++ database/seeders/DistribusiSeeder.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Distribusi;
+use Illuminate\Database\Seeder;
+
+class DistribusiSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $distribusis = [
+            [
+                "sekolah_tujuan" => "SDN 01 Pagi",
+                "jumlah_porsi" => 450,
+                "tanggal_pengiriman" => now()->subDays(3),
+                "status" => "Diterima",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SMPN 15 Jakarta",
+                "jumlah_porsi" => 620,
+                "tanggal_pengiriman" => now()->subDays(3),
+                "status" => "Diterima",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SMA Cendekia",
+                "jumlah_porsi" => 480,
+                "tanggal_pengiriman" => now()->subDays(2),
+                "status" => "Diterima",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SD Muhammadiyah 2",
+                "jumlah_porsi" => 380,
+                "tanggal_pengiriman" => now()->subDays(2),
+                "status" => "Diterima Sebagian",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SDN 01 Pagi",
+                "jumlah_porsi" => 450,
+                "tanggal_pengiriman" => now()->subDays(1),
+                "status" => "Diterima",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SMPN 15 Jakarta",
+                "jumlah_porsi" => 620,
+                "tanggal_pengiriman" => now()->subDays(1),
+                "status" => "Di Perjalanan",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SMA Cendekia",
+                "jumlah_porsi" => 480,
+                "tanggal_pengiriman" => now(),
+                "status" => "Di Perjalanan",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SDN 01 Pagi",
+                "jumlah_porsi" => 450,
+                "tanggal_pengiriman" => now(),
+                "status" => "Di Perjalanan",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SD Muhammadiyah 2",
+                "jumlah_porsi" => 380,
+                "tanggal_pengiriman" => now(),
+                "status" => "Di Perjalanan",
+                "catatan_kendala" => null,
+            ],
+            [
+                "sekolah_tujuan" => "SMPN 15 Jakarta",
+                "jumlah_porsi" => 620,
+                "tanggal_pengiriman" => now(),
+                "status" => "Di Perjalanan",
+                "catatan_kendala" => null,
+            ],
+        ];
+
+        foreach ($distribusis as $distribusi) {
+            Distribusi::firstOrCreate(
+                [
+                    "sekolah_tujuan" => $distribusi["sekolah_tujuan"],
+                    "tanggal_pengiriman" => $distribusi["tanggal_pengiriman"],
+                ],
+                $distribusi,
+            );
+        }
+    }
+}
---
+++ database/seeders/MenuSeeder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Menu;
+use Illuminate\Database\Seeder;
+
+class MenuSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $menus = [
+            [
+                "name" => "Nasi Ayam Bakar + Sayur Sup",
+                "description" =>
+                    "Ayam bakar dengan bumbu kecap manis, disajikan dengan sup sayuran segar",
+                "calories" => 520,
+                "price" => 25000,
+            ],
+            [
+                "name" => "Nasi Telur Dadar + Capcay",
+                "description" =>
+                    "Telur dadar tebal dengan sayuran capcay tumis",
+                "calories" => 480,
+                "price" => 20000,
+            ],
+            [
+                "name" => "Nasi Ikan Lele + Tumis Kangkung",
+                "description" =>
+                    "Ikan lele goreng crispy dengan tumis kangkung saos tiram",
+                "calories" => 510,
+                "price" => 22000,
+            ],
+            [
+                "name" => "Nasi Tempe Goreng + Sayur Asem",
+                "description" =>
+                    "Tempe goreng tepung dengan sayur asem jakarta",
+                "calories" => 450,
+                "price" => 18000,
+            ],
+            [
+                "name" => "Nasi Ayam Suwir + Buncis Tumis",
+                "description" =>
+                    "Ayam suwir bumbu merah dengan buncis tumis bawang",
+                "calories" => 495,
+                "price" => 23000,
+            ],
+            [
+                "name" => "Nasi Ikan Patin + Daun Singkong",
+                "description" =>
+                    "Ikan patin panggang dengan rebusan daun singkong",
+                "calories" => 505,
+                "price" => 24000,
+            ],
+            [
+                "name" => "Nasi Daging Semur + Wortel Bening",
+                "description" =>
+                    "Daging semur kecap spesial dengan wortel bening",
+                "calories" => 560,
+                "price" => 28000,
+            ],
+            [
+                "name" => "Nasi Tahu Goreng + Sup Jagung",
+                "description" => "Tahu goreng filled dengan sup jagung manis",
+                "calories" => 430,
+                "price" => 19000,
+            ],
+            [
+                "name" => "Nasi Pecel Ayam + Urap Sayuran",
+                "description" =>
+                    "Ayam goreng dengan pecel bumbu kacang dan urap sayuran",
+                "calories" => 515,
+                "price" => 23000,
+            ],
+            [
+                "name" => "Nasi Soto Ayam + Perkedel",
+                "description" =>
+                    "Soto ayam jawa dengan kuah kaldu rempah dan perkedel kentang",
+                "calories" => 490,
+                "price" => 22000,
+            ],
+        ];
+
+        foreach ($menus as $menu) {
+            Menu::firstOrCreate(["name" => $menu["name"]], $menu);
+        }
+    }
+}
