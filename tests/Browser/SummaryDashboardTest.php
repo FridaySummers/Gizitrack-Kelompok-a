97015e2d13adbcfe9dc9e0a80bfb850ad4c1a415
nadimiranda07 <nadiamiranda181@gmail.com>
2026-05-04 17:55:50 +0700

    GIZITRACK-23 GIZITRACK-24 GIZITRACK-25 GIZITRACK-26: PBI 23 PBI 24 PBI 25 PBI 26

---
+++ tests/Browser/SummaryDashboardTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class SummaryDashboardTest extends DuskTestCase
+{
+    public function testSummaryDashboard(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->assertSee('Email')
+                ->type('email', 'admin@gizitrack.test')
+                ->type('password', 'password')
+                ->click('button[type="submit"]') 
+                ->waitForLocation('/admin/dashboard')
+                ->assertSee('Selamat Datang, Admin!')
+                ->assertPathIs('/admin/dashboard')
+                ->assertSee('Total Distribusi')
+                ->assertSee('Total Vendor')
+                ->assertSee('Total Sekolah');
+        });
+    }
+}
